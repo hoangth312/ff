38b41b0da83cde48f912ce9da07b6dc4de8dbfc8
Hoangth <hoangth@ecomobile.vn>
2024-12-17 16:00:12 +0700

    first commit

---
+++ FFmpeg/FFmpegApp.swift
@@ -0,0 +1,17 @@
+//
+//  FFmpegApp.swift
+//  FFmpeg
+//
+//  Created by HoangTH on 17/12/24.
+//
+
+import SwiftUI
+
+@main
+struct FFmpegApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
